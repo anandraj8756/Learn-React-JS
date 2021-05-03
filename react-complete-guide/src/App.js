2f31b24f43199f170c973fca1e52bf75f57464a7
Anand Raj <anandallen95@gmail.com>
2021-05-03 17:55:27 +0530

    new component added

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
+
 
 function App() {
   return (
+    <div>
     <h1>Hello Anand!!!!!</h1>
+    <ExpenseItem></ExpenseItem>
+    </div>
   );
 }
 
---
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem(){
+    return <h2>This is New Components!!</h2>
+}
+
+export default ExpenseItem;
