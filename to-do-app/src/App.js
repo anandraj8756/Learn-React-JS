e15ff64bd380544c62dd73bb45ecba55e836a235
Anand Raj <anandallen95@gmail.com>
2021-07-12 23:25:29 +0530

    form created with style

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,10 +1,13 @@
-
+import React from "react";
+import AddUser from "./components/Users/AddUser";
 
 
 function App() {
   return (
     
-      <h1>Hello Anand</h1>
+      <div>
+        <AddUser />
+      </div>
     
   );
 }
